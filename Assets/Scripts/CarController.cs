a7e1a77b7c19630deaffc02e8170311ec1e25dc6
Rodron <alexsp2399@gmail.com>
2021-01-17 21:22:36 +0100

    Running in the 90's

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(InputManager))]
 public class CarController : MonoBehaviour
 {
+    public InputManager input;
+    public List<WheelCollider> throttleWheels;
+    public List<WheelCollider> steeringWheels;
+    public float strengthCoefficient = 2000f;
+    public float maxTurn = 20000f;
+    public float vel = 10f;
+
+    void Start()
+    {        
+        input = GetComponent<InputManager>();
+    }
+    
+    void FixedUpdate()
+    {        
+        foreach (WheelCollider wheel in throttleWheels)
+        {            
+            wheel.motorTorque = strengthCoefficient * Time.deltaTime * input.throttle * vel; 
+        }
+
+        foreach (WheelCollider wheel in steeringWheels)
+        {            
+            wheel.steerAngle = maxTurn * input.steer; 
+        }
+    }
+}
+
+
+
+    /*
     Vector3 deltaPosition;
     Quaternion deltaRotation;
     float vel = .1f;
     float rotVel = .7f;
     Vector3 rot = new Vector3 (0,1,0); 
-    
-  
+
+
     void Update()
     {
         if (Input.GetKey(KeyCode.W)){
@@ -35,4 +65,5 @@ public class CarController : MonoBehaviour
             gameObject.GetComponent<Transform>().rotation = Quaternion.Euler(gameObject.GetComponent<Transform>().rotation.eulerAngles - (rot * rotVel));
         }
     }
-}
+    */
+
