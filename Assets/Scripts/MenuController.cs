153e64f4514f7ae752bb9a3f4b51580a73817bb1
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2021-01-20 04:59:17 +0100

    Menus y skybox

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject botonOpciones;
+    [SerializeField] GameObject botonJugar;
+    [SerializeField] GameObject menuOpciones;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void goToGame(){
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void showOptions(){
+        botonOpciones.SetActive(false);
+        botonJugar.SetActive(false);
+        menuOpciones.SetActive(true);
+    }
+
+    public void hideOptions(){
+
+    }
+}
