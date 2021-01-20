a7683f82091dcc53f904c2daa91264265c343edd
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2021-01-20 06:54:14 +0100

    On my wings

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
@@ -10,9 +11,20 @@ public class MenuController : MonoBehaviour
     [SerializeField] GameObject botonOpciones;
     [SerializeField] GameObject botonJugar;
     [SerializeField] GameObject menuOpciones;
+    [SerializeField] GameObject backButton;
+    [SerializeField] GameObject titulo;
+    [SerializeField] Slider slider;
+    [SerializeField] InputField numeroVueltas;
+
+
+
+    int outLap = 3; 
     void Start()
-    {
+    {   slider.value = PlayerPrefs.GetFloat("volume",1.0f);
         
+        AudioListener.volume = slider.value;
+
+        int outLap = PlayerPrefs.GetInt("laps",3);
     }
 
     // Update is called once per frame
@@ -28,10 +40,28 @@ public class MenuController : MonoBehaviour
     public void showOptions(){
         botonOpciones.SetActive(false);
         botonJugar.SetActive(false);
+        titulo.SetActive(false);
         menuOpciones.SetActive(true);
     }
 
     public void hideOptions(){
 
+        PlayerPrefs.SetFloat("volume",slider.value);
+        int.TryParse(numeroVueltas.text,out outLap);
+        PlayerPrefs.SetInt("laps",outLap);    
+        
+        
+
+        menuOpciones.SetActive(false);
+        botonOpciones.SetActive(true);
+        titulo.SetActive(true);
+        botonJugar.SetActive(true);
     }
+
+    public void changeVolume(){
+        AudioListener.volume = slider.value;
+    }
+
+    
+    
 }
---
+++ Assets/Scripts/goToMnu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class goToMnu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject text;
+    void Start()
+    {
+        text.GetComponent<Text>().text = "Tu puntuación fue de : "+ PlayerPrefs.GetInt("puntuacion")+" puntos";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void goToMenu(){
+        SceneManager.LoadScene("menu");
+    }
+}
